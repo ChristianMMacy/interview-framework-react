ef56beff9380de936e7c69d0bb76bdeda7af012e
Christian Macy <christian@sparrowcompass.com>
2022-08-05 03:41:38 +0000

    add typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {ChakraProvider} from '@chakra-ui/react'
 import {BrowserRouter} from "react-router-dom";
+import {createRoot} from "react-dom/client";
+import React = require('react');
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
